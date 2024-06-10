70224551b1642eba85db3c2a38bb5a646775e252
dharanicb98 <dharanidhamini123@gmail.com>
2024-06-10 10:00:35 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,10 @@ const transactionRoutes = require('./routes/transactionRoutes');
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/roxiler', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/roxiler')
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
+
 app.use('/api/transactions', transactionRoutes);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
